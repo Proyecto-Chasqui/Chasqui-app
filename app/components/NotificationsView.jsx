847377fca913701363ac8b1ca5125f02988c3535
unknown <degnexo@gmail.com>
2020-06-05 19:14:18 -0300

    agregada funcionalidad de marcar todas las notificaciones como leidas

--- app/components/NotificationsView.jsx
+++ app/components/NotificationsView.jsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 import GLOBALS from '../Globals'
 import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler'
 import LoadingView from '../components/LoadingView'
-
+import LoadingOverlayView from '../components/generalComponents/LoadingOverlayView'
 class NotificationsView extends React.PureComponent {
     constructor(props) {
         super(props)
@@ -16,12 +16,14 @@ class NotificationsView extends React.PureComponent {
             notifications: [],
             page: 1,
             totalNotifications: 1,
+            unreadNotifications: this.props.unreadNotifications.length,
+            markingAll:false,
         }
     }
 
-    errorAlert(error){
+    errorAlert(error) {
         if (error.response) {
-            if(error.response.status === 401){
+            if (error.response.status === 401) {
                 Alert.alert(
                     'Sesion expirada',
                     'Su sesión expiro, retornara a los catalogos para reiniciar su sesión',
@@ -30,17 +32,17 @@ class NotificationsView extends React.PureComponent {
                     ],
                     { cancelable: false },
                 );
-            }else{
-                if(error.response.data !== null){
+            } else {
+                if (error.response.data !== null) {
                     Alert.alert(
                         'Error',
-                         error.response.data.error,
+                        error.response.data.error,
                         [
                             { text: 'Entendido', onPress: () => null },
                         ],
                         { cancelable: false },
                     );
-                }else{
+                } else {
                     Alert.alert(
                         'Error',
                         'Ocurrio un error inesperado, sera reenviado a los catalogos. Si el problema persiste comuniquese con soporte tecnico.',
@@ -58,8 +60,8 @@ class NotificationsView extends React.PureComponent {
         }
     }
 
-    componentDidUpdate(){
-        if(this.props.hasReceivedPushNotifications){
+    componentDidUpdate() {
+        if (this.props.hasReceivedPushNotifications) {
             this.restartNotifications()
             this.getUnreadNotifications()
             this.props.actions.hasReceivedPushNotifications(false)
@@ -72,48 +74,48 @@ class NotificationsView extends React.PureComponent {
         this.getTotalNotifications()
     }
 
-    acceptInvitation(notification){
+    acceptInvitation(notification) {
         Alert.alert(
             'Aviso',
             "¿Esta seguro de aceptar la invitación?",
             [
                 { text: 'No', onPress: () => null },
-                { text: 'Si', onPress: () =>  this.sendAccept(notification.id) },
+                { text: 'Si', onPress: () => this.sendAccept(notification.id) },
             ],
             { cancelable: false },
         );
     }
 
-    declineInvitation(notification){
+    declineInvitation(notification) {
         Alert.alert(
             'Aviso',
             "¿Esta seguro de rechazar la invitación?",
             [
                 { text: 'No', onPress: () => null },
-                { text: 'Si', onPress: () =>  this.sendDeclined(notification.id) },
+                { text: 'Si', onPress: () => this.sendDeclined(notification.id) },
             ],
             { cancelable: false },
         );
     }
 
-    restartNotifications(){
-        this.setState({notifications:[],page:1,firstLoading: true});
+    restartNotifications() {
+        this.setState({ notifications: [], page: 1, firstLoading: true });
         this.getFirstNotifications()
         this.getTotalNotifications()
     }
     //la ruta para aceptar una invitacion de nodo es rest/user/nodos/aceptarInvitacion
     //revisar como parametrizar esto.
-    sendAccept(id){
-        axios.post(this.serverBaseRoute + 'rest/user/gcc/aceptar',{
-            idInvitacion : id,
+    sendAccept(id) {
+        axios.post(this.serverBaseRoute + 'rest/user/gcc/aceptar', {
+            idInvitacion: id,
         }, { withCredentials: true }).then(res => {
-            this.markNotification(id,"NOTIFICACION_ACEPTADA");
+            this.markNotification(id, "NOTIFICACION_ACEPTADA");
             this.getUnreadNotifications();
             Alert.alert(
                 'Aviso',
                 "Invitación aceptada!",
                 [
-                    { text: 'Entendido', onPress: () =>  null },
+                    { text: 'Entendido', onPress: () => null },
                 ],
                 { cancelable: false },
             );
@@ -123,7 +125,7 @@ class NotificationsView extends React.PureComponent {
                 'Error',
                 "Ocurrio un error, es probable que la invitación haya sido cancelada por el remitente",
                 [
-                    { text: 'Entendido', onPress: () =>  this.restartNotifications() },
+                    { text: 'Entendido', onPress: () => this.restartNotifications() },
                 ],
                 { cancelable: false },
             );
@@ -131,17 +133,17 @@ class NotificationsView extends React.PureComponent {
     }
     //la ruta para aceptar una invitacion de nodo es rest/user/nodos/rechazarInvitacion
     //revisar como parametrizar esto.
-    sendDeclined(id){
-        axios.post(this.serverBaseRoute + 'rest/user/gcc/rechazar',{
-            idInvitacion : id,
+    sendDeclined(id) {
+        axios.post(this.serverBaseRoute + 'rest/user/gcc/rechazar', {
+            idInvitacion: id,
         }, { withCredentials: true }).then(res => {
-            this.markNotification(id,"NOTIFICACION_RECHAZADA");
+            this.markNotification(id, "NOTIFICACION_RECHAZADA");
             this.getUnreadNotifications();
             Alert.alert(
                 'Aviso',
                 "Invitación rechazada",
                 [
-                    { text: 'Entendido', onPress: () =>  null },
+                    { text: 'Entendido', onPress: () => null },
                 ],
                 { cancelable: false },
             );
@@ -151,7 +153,7 @@ class NotificationsView extends React.PureComponent {
                 'Error',
                 "Ocurrio un error, es probable que la invitación haya sido cancelada por el remitente",
                 [
-                    { text: 'Entendido', onPress: () =>  this.restartNotifications() },
+                    { text: 'Entendido', onPress: () => this.restartNotifications() },
                 ],
                 { cancelable: false },
             );
@@ -161,6 +163,7 @@ class NotificationsView extends React.PureComponent {
     getUnreadNotifications() {
         axios.get(this.serverBaseRoute + 'rest/user/adm/notificacion/noLeidas', { withCredentials: true }).then(res => {
             this.props.actions.unreadNotifications(res.data);
+            this.setState({ unreadNotifications: this.props.unreadNotifications.length, markingAll:false })
         }).catch((error) => {
             this.errorAlert(error)
         });
@@ -197,7 +200,7 @@ class NotificationsView extends React.PureComponent {
                 this.errorAlert(error);
             });
     }
-    
+
     markNotification(id, value) {
         let array = [];
         let notificationsCopy = this.state.notifications
@@ -213,27 +216,98 @@ class NotificationsView extends React.PureComponent {
         this.setState({ loading: false })
     }
 
+
+
+    alertMarkAllNotificiations() {
+        if (this.props.unreadNotifications.length > 0) {
+            Alert.alert(
+                'Pregunta',
+                '¿Desea marcar todas las notificaciones como leidas? (las invitaciones no se verán afectadas)',
+                [
+                    { text: 'No', onPress: () => null },
+                    { text: 'Si', onPress: () => this.executeMarkAll() },
+                ],
+                { cancelable: false },
+            );
+        }else{
+            Alert.alert(
+                'Aviso',
+                'No posee noficaciones sin leer',
+                [
+                    { text: 'Entendido', onPress: null },
+                ],
+                { cancelable: false },
+            );
+        }
+    }
+
+    async alertLongTime(){
+        Alert.alert(
+            'Pregunta',
+            'Tiene demasiadas notificaciones el proceso demorara un tiempo. ¿Desea marcarlas de todas formas?',
+            [
+                { text: 'No', onPress: () => null },
+                { text: 'Si', onPress: () =>  this.markAllNotifications().then(this.fullRestart()) },
+            ],
+            { cancelable: false },
+        );
+    }
+
+    fullRestart(){
+        this.getUnreadNotifications()
+        this.restartNotifications()
+    }
+
+    async executeMarkAll() {
+        if(this.props.unreadNotifications.length < 10){
+            this.markAllNotifications().then(this.fullRestart())   
+        }else{
+            this.alertLongTime()
+        }     
+    }
+
+    async markAllNotifications() {
+        this.setState({ firstLoading: true, markingAll:true })
+        return Promise.all(
+            this.props.unreadNotifications.map((notification, i) => {
+                if (!this.isInvitation(notification.mensaje)) {
+                    this.asyncMarkViewedNotification(notification.id, "Leido");
+                }
+            })
+        )
+    }
+
+    async asyncMarkViewedNotification(id, value) {
+        this.setState({ loading: true })
+        axios.post(this.serverBaseRoute + 'rest/user/adm/notificacion/' + id, {}, { withCredentials: true })
+            .then(res => {
+                //this.markNotification(id, value)
+            }).catch((error) => {
+                console.log(error);
+            });
+    }
+
     markViewedNotification(id, value) {
         this.setState({ loading: true })
         axios.post(this.serverBaseRoute + 'rest/user/adm/notificacion/' + id, {}, { withCredentials: true })
             .then(res => {
-                this.markNotification(id,value)
+                this.markNotification(id, value)
                 this.getUnreadNotifications();
             }).catch((error) => {
                 this.setState({ loading: false })
-               this.errorAlert(error)
+                this.errorAlert(error)
             });
     }
 
     isInvitation(message) {
-        return message.includes('invitado al grupo de compras')|| message.includes('invitado al nodo de compras');
+        return message.includes('invitado al grupo de compras') || message.includes('invitado al nodo de compras');
     }
 
     keyExtractor = (item, index) => index.toString()
 
     renderItem = ({ item }) => (
         <TouchableOpacity onPress={() => this.markViewedNotification(item.id, "Leido")} disabled={this.isInvitation(item.mensaje)} style={styles.notificationItem}>
-            <View style={{ flex: 4,  marginLeft: 20 }}>
+            <View style={{ flex: 4, marginLeft: 20 }}>
                 <View style={{ alingItems: "center", flexDirection: "row" }}>
                     <Text style={{ fontSize: 11 }}>De:</Text>
                     <Text style={{ fontSize: 11, fontWeight: "bold", color: "blue" }}> {item.usuarioOrigen}</Text>
@@ -251,9 +325,9 @@ class NotificationsView extends React.PureComponent {
                 {this.isInvitation(item.mensaje) ? (
                     <View style={{}}>
                         {item.estado === "NOTIFICACION_NO_LEIDA" ? (
-                            <View style={{ marginTop:5, flexDirection: "row", justifyContent: "space-evenly" }}>
+                            <View style={{ marginTop: 5, flexDirection: "row", justifyContent: "space-evenly" }}>
                                 <Button title="Rechazar"
-                                    buttonStyle={{ backgroundColor: "transparent", borderColor: "black", borderWidth: 2, borderRadius:8 }}
+                                    buttonStyle={{ backgroundColor: "transparent", borderColor: "black", borderWidth: 2, borderRadius: 8 }}
                                     titleStyle={{ color: "red" }}
                                     icon={
                                         <Icon
@@ -264,10 +338,10 @@ class NotificationsView extends React.PureComponent {
                                             color='red'
                                         />
                                     }
-                                    onPress={()=>this.declineInvitation(item)}
+                                    onPress={() => this.declineInvitation(item)}
                                 ></Button>
                                 <Button title="Aceptar"
-                                    buttonStyle={{ backgroundColor: "transparent", borderColor: "black", borderWidth: 2 ,borderRadius:8}}
+                                    buttonStyle={{ backgroundColor: "transparent", borderColor: "black", borderWidth: 2, borderRadius: 8 }}
                                     titleStyle={{ color: "green" }}
                                     icon={<Icon
                                         containerStyle={{ marginEnd: 5 }}
@@ -276,7 +350,7 @@ class NotificationsView extends React.PureComponent {
                                         type='font-awesome'
                                         color='green'
                                     />}
-                                    onPress={()=>this.acceptInvitation(item)}
+                                    onPress={() => this.acceptInvitation(item)}
                                 ></Button>
                             </View>) : (null)}
                     </View>
@@ -360,6 +434,9 @@ class NotificationsView extends React.PureComponent {
                                     <Icon name="bell" size={20} color="white" type='font-awesome' />
                                 }
                                 buttonStyle={styles.rightHeaderButton}
+                                loading={this.state.loading}
+                                disabled={this.state.loading}
+                                onPress={() => this.alertMarkAllNotificiations()}
                             />
                             {this.props.unreadNotifications.length > 0 ? (
                                 <Badge value={this.props.unreadNotifications.length} status="error" containerStyle={{ position: 'absolute', top: -6, right: -6 }} />
@@ -367,35 +444,36 @@ class NotificationsView extends React.PureComponent {
                         </View>
                     </Header>
                 </View>
+                <LoadingOverlayView isVisible={this.state.markingAll} loadingText="Comunicandose con el servidor..." ></LoadingOverlayView>
                 {this.state.firstLoading ? (<LoadingView></LoadingView>) : (
-                <View style={{ flex: 1 }}>
-                    {this.state.notifications.length === 0 && !this.state.loading ? (
-                        <View style={styles.viewErrorContainer}>
-                            <View style={styles.searchIconErrorContainer}>
-                                <Icon name="bell" type='font-awesome' size={50} color={"white"} containerStyle={styles.searchIconError}></Icon>
-                            </View>
-                            <Text style={styles.errorText}>
-                                No hay notificaciones
+                    <View style={{ flex: 1 }}>
+                        {this.state.notifications.length === 0 && !this.state.loading ? (
+                            <View style={styles.viewErrorContainer}>
+                                <View style={styles.searchIconErrorContainer}>
+                                    <Icon name="bell" type='font-awesome' size={50} color={"white"} containerStyle={styles.searchIconError}></Icon>
+                                </View>
+                                <Text style={styles.errorText}>
+                                    No hay notificaciones
                             </Text>
-                            <Text style={styles.tipErrorText}>
-                                No posee notificaciones hasta el momento
+                                <Text style={styles.tipErrorText}>
+                                    No posee notificaciones hasta el momento
                             </Text>
-                        </View>) : (
-                            <View style={{ flex: 1 }}>
-                                <FlatList
-                                    ListHeaderComponent={
-                                        <View style={styles.titleContainer}>
-                                            <Text style={styles.adressTitle}>Notificaciones</Text>
-                                        </View>}
-                                    keyExtractor={this.keyExtractor}
-                                    data={this.state.notifications}
-                                    renderItem={(item) => this.renderItem(item)}
-                                    ListFooterComponent={
-                                        <Button titleStyle={{ fontSize: 20, color: 'white' }} buttonStyle={styles.buttonMore} loading={this.state.loading} containerStyle={{ margin: 5 }} onPress={() => this.addNotification()} disabled={this.state.notifications.length >= this.state.totalNotifications} title={"Ver mas notificaciones"}></Button>
-                                    }
-                                />
-                            </View>)}
-                </View>
+                            </View>) : (
+                                <View style={{ flex: 1 }}>
+                                    <FlatList
+                                        ListHeaderComponent={
+                                            <View style={styles.titleContainer}>
+                                                <Text style={styles.adressTitle}>Notificaciones</Text>
+                                            </View>}
+                                        keyExtractor={this.keyExtractor}
+                                        data={this.state.notifications}
+                                        renderItem={(item) => this.renderItem(item)}
+                                        ListFooterComponent={
+                                            <Button titleStyle={{ fontSize: 20, color: 'white' }} buttonStyle={styles.buttonMore} loading={this.state.loading} containerStyle={{ margin: 5 }} onPress={() => this.addNotification()} disabled={this.state.notifications.length >= this.state.totalNotifications} title={"Ver mas notificaciones"}></Button>
+                                        }
+                                    />
+                                </View>)}
+                    </View>
                 )}
             </View>
         )
